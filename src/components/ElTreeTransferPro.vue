c8cc8a5260151b432638e42b6875422f81196e1d
PersiLiao <xiangchu.liao@gmail.com>
2023-09-01 02:04:21 +0800

    refactor: Code optimization

---
+++ src/components/ElTreeTransferPro.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="tree-transfer-container">
+    <el-tree-transfer-select :data="leftData" :node-key="nodeKey" :pid-key="pidKey" :title="leftTitle" v-model="leftCheckedValue" />
+    <div class="transfer-button">
+      <el-button :disabled="toLeftDisabled" :icon="ArrowLeft" type="primary" @click="transferToLeft"></el-button>
+      <el-button :disabled="toRightDisabled" :icon="ArrowRight" type="primary" @click="transferToRight"></el-button>
+    </div>
+    <el-tree-transfer-select :data="rightData" :node-key="nodeKey" :pid-key="pidKey" :title="rightTitle" v-model="rightCheckedValue" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ArrowLeft, ArrowRight } from '@element-plus/icons-vue'
+import { computed, ref, watch } from 'vue'
+import ElTreeTransferSelect from './ElTreeTransferSelect.vue'
+
+const props = defineProps({
+  dataSource: {
+    type: Array<any>,
+    required: true,
+    default: []
+  },
+  defaultProps: {
+    type: Object,
+    default: () => {
+      return {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  nodeKey: {
+    type: String,
+    default: 'id'
+  },
+  pidKey: {
+    type: String,
+    default: 'pid'
+  },
+  titles: {
+    type: Array<String>,
+    default: ['待选', '已选']
+  }
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+const leftTitle = computed(() => {
+  return props.titles[0]
+})
+const leftData = ref([])
+const leftCheckedValue = ref(null)
+const leftCheckKeys = computed(() => {
+  return leftCheckedValue.value?.keys ?? []
+})
+
+const toLeftDisabled = computed(() => {
+  return (rightData.value && rightData.value?.length === 0) || (rightCheckKeys.value && rightCheckKeys.value.length === 0)
+})
+const transferToLeft = () => {
+  leftData.value = JSON.parse(JSON.stringify(rightCheckedValue.value.nodes))
+}
+
+const rightTitle = computed(() => {
+  return props.titles[1]
+})
+const rightData = ref([])
+const rightCheckedValue = ref(null)
+const rightCheckKeys = computed(() => {
+  return rightCheckedValue.value?.keys ?? []
+})
+
+const toRightDisabled = computed(() => {
+  return (leftData.value && leftData.value?.length === 0) || (leftCheckKeys.value && leftCheckKeys.value.length === 0)
+})
+const transferToRight = () => {
+  rightData.value = JSON.parse(JSON.stringify(leftCheckedValue.value.nodes))
+}
+
+watch(() => props.dataSource, (newDataSource) => {
+  leftData.value = JSON.parse(JSON.stringify(newDataSource))
+}, { immediate: true })
+
+</script>
+
+<style lang="scss" scoped>
+.tree-transfer-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
---
+++ src/components/ElTreeTransferSelect.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="container">
+    <div class="title">
+      <el-checkbox v-model="checkAll" @change="checkboxClick" :disabled="checkAllDisabled" :indeterminate="isIndeterminate" />
+      <el-text>{{ title }}</el-text>
+      <el-text class="total">{{ checkedLength }}/{{ treeLength }}</el-text>
+    </div>
+    <div class="tree-container">
+      <el-tree ref="elTreeRef" :data="data" :node-key="nodeKey" :props="defaultProps" show-checkbox
+               @check="updateCheckedKeys" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ElTree } from 'element-plus'
+import { computed, onMounted, ref, watch } from 'vue'
+
+const props = defineProps({
+  modelValue: [Object],
+  title: {
+    type: String,
+    required: true
+  },
+  isIndeterminate: {
+    type: Boolean,
+    default: false
+  },
+  data: {
+    type: Array<any>,
+    required: true,
+    default: []
+  },
+  defaultProps: {
+    type: Object,
+    default: () => {
+      return {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  nodeKey: {
+    type: String,
+    default: 'id'
+  },
+  pidKey: {
+    type: String,
+    default: 'pid'
+  }
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+const elTreeRef = ref(null)
+
+const treeKeys = ref([])
+const treeLength = computed(() => {
+  return treeKeys.value.length
+})
+
+const checkAll = ref(false)
+const checkAllDisabled = computed(() => {
+  return treeLength.value === 0
+})
+
+const checkedKeys = ref([])
+const checkedLength = computed(() => {
+  return checkedKeys.value.length
+})
+
+const setCheckAll = (checked: boolean) => {
+  checkAll.value = checked
+}
+
+const setTreeKeys = (trees: any[]) => {
+  for (const tree of trees) {
+    treeKeys.value.push(tree)
+    if (tree[props.defaultProps?.children]) {
+      setTreeKeys(tree[props.defaultProps?.children])
+    }
+  }
+}
+
+const updateCheckedKeys = () => {
+  checkedKeys.value = elTreeRef.value.getCheckedKeys(false, true)
+  emits('update:modelValue', {
+    keys: checkedKeys.value,
+    nodes: elTreeRef.value.getCheckedNodes(false)
+  })
+}
+
+const checkboxClick = (checked: boolean) => {
+  if (checked) {
+    elTreeRef.value.setCheckedNodes(props.data)
+  } else {
+    elTreeRef.value.setCheckedNodes([])
+  }
+  updateCheckedKeys()
+}
+
+watch(checkedLength, (newCheckedLength) => {
+  setCheckAll(treeLength.value === newCheckedLength)
+})
+
+watch(() => props.data, (newData) => {
+  setTreeKeys(newData)
+})
+
+onMounted(() => {
+  setTreeKeys(props.data)
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
